520b4aa498ff46695d9690aefcd1d1aa06e33229
Elena Baginskaya <elenabor@list.ru>
2022-11-15 23:43:57 +0300

    task_1 done 4

--- task_1/solution.js
+++ task_1/solution.js
@@ -4,9 +4,18 @@ function addInCartMessage(title, price) {
 
     // Задание №1.1. Оповещение о добавлении в корзину
    
-    let message = productName + " за " + productPrice + " теперь в корзине!"; 
+    let message;
+    message = productName;
+    message = message + “ ”;
+    message = message + “за”; 
+    message = message + “ ”;
+    message += productPrice;
+    message = message + “ ”;
+    message = message + теперь в корзине!;
+
 
     console.log(message); 
+    
    
     // Конец решения задания №1.1.
 
