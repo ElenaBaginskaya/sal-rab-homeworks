cc195237d19513ff53f4b31ced5193cdcf078a79
Elena Baginskaya <elenabor@list.ru>
2022-11-16 02:16:53 +0300

    task_2 done 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -11,15 +11,15 @@ function calcShipping(sum, min, shipping) {
     // то shippingSum присвоить значение 0
     if (productsSum == 0){
         shippingSum = 0;
-    }
+    } else
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
     if (productsSum >= freeShippingMinSum){
         shippingSum = 0;
-    }
+    }else
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-    if (productsSum > 0 && productsSum < shippingPrice){
+    if ((productsSum > 0) & (productsSum < freeShippingMinSum)){
         shippingSum = shippingPrice;
     }
     // Конец решения задания №2.1.
